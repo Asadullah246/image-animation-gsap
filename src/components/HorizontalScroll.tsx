06931f6d316db56b5babb22bd302eefa2a1dc8a0
Asadullah246 <asadm2258@gmail.com>
2023-10-28 12:00:11 +0600

    up

--- src/components/HorizontalScroll.tsx
+++ src/components/HorizontalScroll.tsx
@@ -2,36 +2,12 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import React, { useEffect, useRef } from 'react';
 
-gsap.registerPlugin(ScrollTrigger);
+
 
 const HorizontalScrollComponent: React.FC = () => {
   const sectionPinRef = useRef<HTMLDivElement | null>(null);
 
-  // useEffect(() => { 
-  //   const sectionPin = sectionPinRef?.current;
-
-  //   if (sectionPin) {
-  //     const containerAnimation = gsap.to(sectionPin, {
-  //       scrollTrigger: {
-  //         trigger: sectionPin,
-  //         start: 'top top',
-  //         end: () => `+=${sectionPin.offsetWidth}`,
-  //         pin: true,
-  //         scrub: true,
-  //       },
-  //       x: () =>
-  //         `-${sectionPin.scrollWidth - document.documentElement.clientWidth}px`,
-  //       ease: 'none',
-  //     });
-
-  //     return () => {
-  //       containerAnimation.kill();
-  //     };
-  //   }
-
-  // }, []);
-
-
+ 
 
   return (
     <div className="flex h-screen flex-col">
