85758f1cc868d8f595a91ae10e29dff2bdf88382
Asadullah246 <asadm2258@gmail.com>
2023-10-28 12:09:07 +0600

    d

--- src/components/HorizontalScroll.tsx
+++ src/components/HorizontalScroll.tsx
@@ -1,26 +1,9 @@
-import React, { useRef } from 'react';
+import React from 'react';
 
 const HorizontalScrollComponent: React.FC = () => {
-  const sectionPinRef = useRef<HTMLDivElement | null>(null);
-
   return (
-    <div className="flex h-screen flex-col">
-      <div
-        id="section_to-pin"
-        className="flex h-screen items-center justify-center bg-gray-200"
-      >
-        Scroll down to trigger horizontal scrolling
-      </div>
-      <div
-        id="section_pin"
-        ref={sectionPinRef}
-        className="flex-none overflow-x-scroll whitespace-nowrap"
-      >
-        <div className="h-screen w-screen bg-blue-400">Image 1</div>
-        <div className="h-screen w-screen bg-red-400">Image 2</div>
-        <div className="h-screen w-screen bg-green-400">Image 3</div>
-        {/* Add more image divs with Tailwind CSS classes as needed */}
-      </div>
+    <div className="">
+      <p>something</p>
     </div>
   );
 };
